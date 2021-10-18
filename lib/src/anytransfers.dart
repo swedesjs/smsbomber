f0a9d35d13d1034fad585ddea8bc792eb82e0650
swedesjs <babaevvladislav828@gmail.com>
2021-10-18 16:48:45 +0300

    formatting

--- lib/src/anytransfers.dart
+++ lib/src/anytransfers.dart
@@ -7,18 +7,27 @@ const countyList = {"RU": Country.RU};
 
 final serviceList = [
   Service(
-      name: "Citilink",
-      url: Uri(
-          scheme: "https", host: "www.citilink.ru", path: "registration/confirm/phone/+7{phone}"),
-      country: Country.RU,
-      process: _serviceProcessFunctionDefault),
+    name: "Citilink",
+    url: Uri(
+      scheme: "https",
+      host: "www.citilink.ru",
+      path: "registration/confirm/phone/+7{phone}",
+    ),
+    country: Country.RU,
+    process: _serviceProcessFunctionDefault,
+  ),
   Service(
     name: "Tele2",
-    url: Uri(scheme: "https", host: "msk.tele2.ru", path: "api/validation/number/7{phone}"),
+    url: Uri(
+      scheme: "https",
+      host: "msk.tele2.ru",
+      path: "api/validation/number/7{phone}",
+    ),
     country: Country.RU,
     process: (service, phone) => _dio.post(
-        service.url.toString().replaceFirst("%7Bphone%7D", "$phone"),
-        data: jsonEncode(const {"sender": "Tele2"})),
+      service.url.toString().replaceFirst("%7Bphone%7D", "$phone"),
+      data: jsonEncode(const {"sender": "Tele2"}),
+    ),
   )
 ];
 
